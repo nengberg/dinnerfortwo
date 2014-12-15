53f4e1c3e7339f270a6d93627bc116b2a4702ed6
nengberg <per.niklas.engberg@gmail.com>
2014-12-15 22:21:36 +0100

    Overriding more html templates from django.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from recipes.models import Recipe, Ingredient
 from django_summernote.admin import SummernoteModelAdmin
+from django.http import HttpResponse
+from django.conf.urls import patterns
 
 class IngredientInline(admin.StackedInline):
 	model = Ingredient
-	extra = 1
+	extra = 0
 
 class RecipeAdmin(SummernoteModelAdmin):
 	inlines = [IngredientInline]
-
 
 admin.site.register(Recipe, RecipeAdmin)
