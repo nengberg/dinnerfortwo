9fb995265cb156ac2764202d8040fc013fdc7201
nengberg <per.niklas.engberg@gmail.com>
2014-12-23 10:24:55 +0100

    Fixed button markup, added more html files from django template.

---
+++ recipes/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from django.db import models
+from django.forms import ModelForm
+
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.db import models
-from django.forms import ModelForm
 from multiselectfield import MultiSelectField
+
 
 CATEGORY_CHOICES = (
     (1, 'Svenskt'),
@@ -11,12 +12,12 @@ CATEGORY_CHOICES = (
 )
 
 class Recipe(models.Model):
-	title = models.CharField(max_length=300)
-	categories = MultiSelectField(choices=CATEGORY_CHOICES, default=1)
-	image = models.ImageField(upload_to='pictures/', null=True)
-	description = models.TextField(default='')
+	title = models.CharField(max_length=300, verbose_name='Namn')
+	categories = MultiSelectField(choices=CATEGORY_CHOICES, default=1, verbose_name='Kategorier')
+	image = models.ImageField(upload_to='pictures/', null=True, verbose_name='Bild')
+	description = models.TextField(default='', verbose_name='Instruktioner')
 	def __str__(self):
-		return 'Namn'
+		return self.title
 
 class Ingredient(models.Model):
 	recipe = models.ForeignKey(Recipe)
@@ -25,7 +26,13 @@ class Ingredient(models.Model):
 	def __str__(self):
 		return 'Ingrediens'
 
-class RecipeForm(ModelForm):
-    class Meta:
-        model = Recipe
-        fields = ['title', 'categories', 'description']
+
+class RecipeForm(forms.ModelForm):
+
+	class Meta:
+		model = Recipe
+		fields = ['title', 'categories', 'description']
+		title = forms.CharField(label='Your name')
+
+class RecipeForm(forms.ModelForm):
+	title = forms.CharField(widget=forms.Textarea)
